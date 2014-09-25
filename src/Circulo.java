52c213700c249d4c85a5b82a77f57aba3d83f0b8
tarzelus <tarzeluset14dw@ikzubirimanteo.com>
2014-09-25 13:16:01 +0200

    first commit

---
+++ src/Circulo.java
@@ -0,0 +1,36 @@
+/*
+ * clase para calcular la circunferencia y el area del circulo
+ */
+public class Circulo {
+
+double radio1=0;
+double result;
+ //Método para calcular la circunferencia del circulo
+ public double circun () {
+
+	double result = 0;
+	result = 6.283185307179586476925286766559 * getRadio();
+	return result;
+ }
+
+
+
+ //Método para calcular el area del circulo mediante getRadio
+ public double area () {
+
+	double result = 0;
+	result = 3.1415926535897932384626433832795 * getRadio()* getRadio();
+	return result;
+ }
+
+//declaracion de los get y set
+private double getRadio(){
+	return radio1;
+}
+public void  setRadio(double radio){
+
+radio1=radio;
+}
+
+}
+
